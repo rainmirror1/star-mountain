f8ace77b0ff2898c176349f60ca4be8e8f6951bc
atiwat-kho <atiwat.kho@gmail.com>
2015-04-27 18:35:48 +0700

    Add the real star mountain

--- star.js
+++ star.js
@@ -1,3 +1,15 @@
+function real_star_mountain() {
+    for(var i=0; i<10; i++) {
+        for(var j=10 - i; j>=0; j--) {
+            document.write('&nbsp;');
+        }
+        for(var j=0; j<i+1; j++) {
+            document.write('*');
+        }        
+        document.write('<br>');
+     }
+}
+
 function star1() {
     for(var i=0; i<10; i++) {
         for(var j=0; j<=i; j++) {
@@ -8,4 +20,5 @@ function star1() {
 }
 window.onload = function () {
     star1();
+    real_star_mountain();
 }
